6b48842614ab1e8c78ad3139866d2aa92646fe77
merseyviking <mersey.viking@gmail.com>
2014-09-12 14:07:08 +0200

    Got it working!

--- static/saunopportunist.js
+++ static/saunopportunist.js
@@ -1,3 +1,41 @@
 /**
  * Created by merseyviking on 12/09/14.
  */
+
+var appId='91d69733-4248-4d52-bf46-55e19e98c9a4';
+var microphoneDeviceId='00b465ae-640d-4a9e-99fc-ee11bfe4a476';
+var token = 'Bearer H1b3ut30GfZahmlU.CiKukBVl-kgv5Ia';
+
+$(function() {
+    $.ajax({
+        url: "https://api.relayr.io/devices/"+ microphoneDeviceId + '/subscription',
+        type: 'post',
+        headers: {
+          "Authorization": token
+        },
+        dataType: 'application/json',
+        complete: function (credentials) {
+            console.log(credentials.responseText);
+            var cred = JSON.parse(credentials.responseText);
+
+              var pubnub = PUBNUB.init({
+                ssl: true,
+                subscribe_key : cred.subscribeKey,
+                cipher_key: cred.cipherKey,
+                auth_key: cred.authKey
+              });
+
+            pubnub.subscribe({
+            channel : cred.channel,
+
+            message : function(data) {
+              //Output the realtime data coming from the device
+//              document.write(data);
+                console.log(data);
+                }
+              });
+
+        }
+    });
+
+});
